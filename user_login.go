e825845ab4404c9ee6915f09c283159d1f45239c
Raj <mraj@hq.corp.viasat.com>
2019-08-06 15:47:22 +0530

    Adding user_login details

---
+++ user_login.go
@@ -0,0 +1,34 @@
+package main
+
+import ("fmt")
+func main() {
+    
+	userInterfaceScreen1()
+
+	}
+
+func userInterfaceScreen1() {
+	fmt.Println("New User press 7 to register:")
+	fmt.Println("Enter Username:")
+	fmt.Println("Enter password:")
+	
+	
+}
+
+func usernamePasswordProcess() {
+
+
+}
+
+func new_user_reg() {
+
+
+}
+
+func plan() {
+	fmt.Println("Program to create User login")
+	fmt.Println("Step1: Create a user login screen - Monday")
+	fmt.Println("Step2 Program a new user scenario - Monday")
+	fmt.Println("Step3 Exisitng user scenario - Tuesday")
+	fmt.Println("Step4 Test for concurrency - Tuesday/Wednesday")
+}
